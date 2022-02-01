f57bf4628e0e257c3ffff6637fe10c833ff1ba35
rishard <rrdavis777@gmail.com>
2022-01-31 21:19:00 -0500

    commit tree cfe4a9806cd8e31139c1e37f135e0e5a0fe1ea2d

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+  let result
+  if (someNumber <= 400) {
+    result = 'This one is on me!';
+  }
+  if (someNumber >= 2000) { 
+    result = 'I will gladly take your thirty bucks.';
+  }
+  if (someNumber > 2500) {
+    result = 'No can do.';
+  }
+  return result 
 }
 
 function ternaryCheckCity(){
   // Write your code here!
+
 }
 
 function switchOnCharmFromTip(){
